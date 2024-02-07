64f0d530a5b4ff8c933761a4b0b20d325cbe4ad5
SkillBridge-Projects <149353936+SkillBridge-Projects@users.noreply.github.com>
2024-02-07 12:05:29 +0530

    commented test classes

--- SkillProofAPI/SkillProofAPI/src/test/java/com/linkedin/linkedinclone/LinkedinCloneApplicationTests.java
+++ SkillProofAPI/SkillProofAPI/src/test/java/com/linkedin/linkedinclone/LinkedinCloneApplicationTests.java
@@ -1,13 +1,13 @@
-package com.linkedin.linkedinclone;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LinkedinCloneApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.linkedin.linkedinclone;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LinkedinCloneApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
